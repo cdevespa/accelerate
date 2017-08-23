62f19a76779c7df5bf95537954fbdcca7e992ba5
cdevespa <cdevespa@gmail.com>
2017-08-22 22:30:17 -0700

    front-page fix

--- wp-content/themes/accelerate-theme-child/front-page.php
+++ wp-content/themes/accelerate-theme-child/front-page.php
@@ -16,10 +16,18 @@ get_header(); ?>
 	
 
 	<div id="primary" class="home-page hero-content">
-	  <section class="recent-posts">
+	<div class="main-content" role="main">
+     <?php while ( have_posts() ) : the_post(); ?>
+       <?php the_content(); ?>
+       <a class="button" href="<?php echo site_url('/blog/') ?>">View Our Work</a>
+     <?php endwhile; // end of the loop. ?>
+   </div><!-- .main-content -->
+ </div><!-- #primary -->
+
+	 <section class="recent-posts">
  		  <div class="site-content">
  		  	<div class="blog-post">
- 		  		<h2>From the Blog</h2>
+ 		  		<h4>From the Blog</h4>
  		  			<?php query_posts('posts_per_page=1'); ?>
      				<?php while ( have_posts() ) : the_post(); ?>
        			<h3><?php the_title(); ?><h3>
@@ -30,7 +38,7 @@ get_header(); ?>
  	     </div>
 	  </section>
 	
-		</div><!-- .main-content -->
-	</div><!-- #primary -->
+		
+	
 
 <?php get_footer(); ?>
