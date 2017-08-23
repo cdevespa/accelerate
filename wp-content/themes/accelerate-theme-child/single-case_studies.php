d6948c83464386180074c5849dd02d7387777293
cdevespa <cdevespa@gmail.com>
2017-08-23 14:34:27 -0700

    got rid of h2 title

--- wp-content/themes/accelerate-theme-child/single-case_studies.php
+++ wp-content/themes/accelerate-theme-child/single-case_studies.php
@@ -17,7 +17,7 @@ get_header(); ?>
 	<div id="primary" class="site-content">
 		<div class="main-content" role="main">
 			<?php while ( have_posts() ) : the_post(); ?>
-				<h2>This is my Content Page</h2>
+				
 				<?php the_content(); ?>
 			<?php endwhile; // end of the loop. ?>
 		</div><!-- .main-content -->
