531908108b3016a63565f88f5d34cb9241adc224
cdevespa <cdevespa@gmail.com>
2017-09-04 17:18:57 -0700

    archive added front page changes

--- wp-content/themes/accelerate-theme-child/front-page.php
+++ wp-content/themes/accelerate-theme-child/front-page.php
@@ -20,33 +20,27 @@ get_header(); ?>
    <div class="main-content" role="main">
      <?php while ( have_posts() ) : the_post(); ?>
        <?php the_content(); ?>
-       <a class="button" href="<?php echo site_url('/blog/') ?>">View Our Work</a>
+       <a class="button" href="<?php echo site_url('/case-studies/') ?>">View Our Work</a>
      <?php endwhile; // end of the loop. ?>
    </div><!-- .main-content -->
  </div><!-- #primary -->
  <section class="featured-work">
- <div class="site-content">
+ <div class="site-content ">
     <h4>Featured Work</h4>
 
     <ul class="homepage-featured-work">
     <?php query_posts('posts_per_page=3&post_type=case_studies'); ?>
          <?php while ( have_posts() ) : the_post(); 
-              $featured_image_1 = get_field('featured_image_1');
-              $featured_image_2 = get_field('featured_image_2');
-              $featured_image_3 = get_field('featured_image_3');
-              $size = "medium"
+              $image_1 = get_field("image_1");
+              $size = "medium";
           ?>
         <li class="individual-featured-work">
           <figure>
-            <?php echo wp_get_attachment_image($featured_image_1, $size); ?>
+            <?php echo wp_get_attachment_image($image_1, $size); ?>
           
-            <?php echo wp_get_attachment_image($featured_image_2, $size); ?>
-           
-            <?php echo wp_get_attachment_image($featured_image_3, $size); ?>
           </figure>
-
+          <h3><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h3>
         </li>
-          <h3><a href="<?php the_permalink(); ?>"</a><?php the_title(); ?></a></h3>
          <?php endwhile; ?> 
        <?php wp_reset_query(); ?>
     </ul>
@@ -57,7 +51,7 @@ get_header(); ?>
        <h4>From the Blog</h4>
        <?php query_posts('posts_per_page=1'); ?>
          <?php while ( have_posts() ) : the_post(); ?>
-          <h3><?php the_title(); ?></h3>
+          <h2><?php the_title(); ?></h2>
           <?php the_excerpt(); ?> 
          <?php endwhile; ?> 
        <?php wp_reset_query(); ?>
