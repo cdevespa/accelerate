4d320f200b77cfae0697520c1a8d785e8b98277d
cdevespa <cdevespa@gmail.com>
2017-09-07 12:07:35 -0700

    about page

---
+++ wp-content/themes/accelerate-theme-child/about.php
@@ -0,0 +1,27 @@
+<?php
+/**
+ * Template Name: About
+ * The template for displaying about page
+ * This is the template that displays the about page
+ *
+ * @package WordPress
+ * @subpackage Accelerate Marketing
+ * @since Accelerate Marketing 2.0
+ */
+
+get_header(); ?>
+
+
+	<div id="primary" class="site-content">
+		<div class="main-content" role="main">
+			<?php while ( have_posts() ) : the_post(); ?>
+				<?php the_content(); ?>
+			<?php endwhile; // end of the loop. ?>
+		</div><!-- .main-content -->
+
+	</div><!-- #primary -->
+
+<?php get_footer(); ?>
+
+
+
