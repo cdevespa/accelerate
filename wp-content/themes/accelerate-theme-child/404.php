7e8b89adc5167150dc17b3972646ef39e7cb2e4d
cdevespa <cdevespa@gmail.com>
2017-09-14 15:16:01 -0700

    404 page

--- wp-content/themes/accelerate-theme-child/404.php
+++ wp-content/themes/accelerate-theme-child/404.php
@@ -10,10 +10,10 @@
                         
                 <article id="post-0" <?php post_class( 'et_pb_post not_found' ); ?>>
                     <h1><?php esc_html_e('OOPS!!! You lost it!','Divi'); ?></h1>
-                    <p><?php esc_html_e('Looks like the page you were looking for doesn\'t exit. Try searching for something else using the search bar above', 'Divi'); ?></p>
+                    <p><?php esc_html_e('Looks like the page you were looking for doesn\'t exit. Try searching for something else using the search bar below.', 'Divi'); ?></p>
                     
                 </article> <!-- .et_pb_post -->
-                
+
             </div> <!-- #left-area -->
 
         </div> <!-- #content-area -->
